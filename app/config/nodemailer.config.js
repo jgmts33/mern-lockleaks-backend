76401643bc3e6602c4a7571527e290b01dd0df39
jgmts33 <jorgreemts@gmail.com>
2024-04-23 20:33:20 +0000

    add: sending email while register

--- app/config/nodemailer.config.js
+++ app/config/nodemailer.config.js
@@ -1,9 +1,9 @@
 export default {
-  host: "smtp.ethereal.email",
-  port: 587,
+  host: "smtp.elasticemail.com",
+  port: 2525,
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: "support@copyrightfixer.com",
-    pass: "@!#0ds@!3psdP",
+    pass: "B7B036FFA732F5100DEC0AA724B32E46B645",
   },
 }
