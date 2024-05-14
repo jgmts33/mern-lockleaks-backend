35a1e0c5de108b29fca2f6b398ec5066ef01d70f
jgmts33 <jorgreemts@gmail.com>
2024-05-14 15:20:09 +0100

    update: scrape api

--- app/controllers/scrape.controller.js
+++ app/controllers/scrape.controller.js
@@ -32,7 +32,7 @@ export const scrapeData = async (req, res) => {
       user_id: id,
     };
 
-    await usernames.map(async (eachData) => {
+    usernames.map(async (eachData) => {
       const customKeyword = await CustomKeywords.findOne({
         where: {
           website: extractDomain(eachData.link)
@@ -51,55 +51,54 @@ export const scrapeData = async (req, res) => {
         await array_keywords.map((item) => fullQuery += `${eachData.username} ${item.keyword}, `);
         fullQuery = fullQuery.slice(0, -2);
       }
-    })
+    }).then(async () => {
+      const queries = fullQuery.replace(",", " ").split("  ");
+      const currentDate = new Date().toLocaleString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }).replace(/[/,:]/g, '-').replace(/\s/g, '_');
 
+      console.log("fullQuery:", fullQuery);
+      console.log("queries:", queries);
+      console.log("currentDate:", currentDate);
 
-    const queries = fullQuery.replace(",", " ").split("  ");
-    const currentDate = new Date().toLocaleString('en-GB', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }).replace(/[/,:]/g, '-').replace(/\s/g, '_');
+      queries.map(async (query) => {
+        const res = await axios.post(`${process.env.BOT_API_ENDPOINT}/scrape`, {
+          query,
+          currentDate
+        });
 
-    console.log("fullQuery:", fullQuery);
-    console.log("queries:", queries);
-    console.log("currentDate:", currentDate);
+        data = {
+          currentDate: currentDate,
+          google_link_count: data.google_link_count + res.data.google_link_count,
+          google_image_count: data.google_image_count + res.data.google_image_count,
+          google_video_count: data.google_video_count + res.data.google_video_count,
+          bing_link_count: data.bing_link_count + res.data.bing_link_count,
+          bing_image_count: data.bing_image_count + res.data.bing_image_count,
+          bing_video_count: data.bing_video_count + res.data.bing_video_count,
+          good_count: data.good_count + res.data.good_count,
+          other_count: data.other_count + res.data.other_count,
+          bad_count: data.bad_count + res.data.bad_count,
+          new_count: data.new_count + res.data.new_count,
+          report_count: data.report_count + res.data.report_count,
+          no_report_count: data.no_report_count + res.data.no_report_count,
+          matches_count: data.matches_count + res.data.matches_count,
+          no_matches_count: data.no_matches_count + res.data.no_matches_count,
+          user_id: id
+        }
 
-    queries.map(async (query) => {
-      const res = await axios.post(`${process.env.BOT_API_ENDPOINT}/scrape`, {
-        query,
-        currentDate
       });
 
-      data = {
-        currentDate: currentDate,
-        google_link_count: data.google_link_count + res.data.google_link_count,
-        google_image_count: data.google_image_count + res.data.google_image_count,
-        google_video_count: data.google_video_count + res.data.google_video_count,
-        bing_link_count: data.bing_link_count + res.data.bing_link_count,
-        bing_image_count: data.bing_image_count + res.data.bing_image_count,
-        bing_video_count: data.bing_video_count + res.data.bing_video_count,
-        good_count: data.good_count + res.data.good_count,
-        other_count: data.other_count + res.data.other_count,
-        bad_count: data.bad_count + res.data.bad_count,
-        new_count: data.new_count + res.data.new_count,
-        report_count: data.report_count + res.data.report_count,
-        no_report_count: data.no_report_count + res.data.no_report_count,
-        matches_count: data.matches_count + res.data.matches_count,
-        no_matches_count: data.no_matches_count + res.data.no_matches_count,
-        user_id: id
-      }
+      const scrapeSummary = await ScrapeSummary.create({ ...data });
 
-    });
-
-    const scrapeSummary = await ScrapeSummary.create({ ...data });
-
-    res.status(200).send({
-      ...scrapeSummary
-    });
+      res.status(200).send({
+        ...scrapeSummary
+      });
+    })
 
   } catch (err) {
     res.status(500).send({
