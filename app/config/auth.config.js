e42f4eb7fbcd3aa00cf4c1345e825370761f7400
jgmts33 <jorgreemts@gmail.com>
2024-05-16 20:16:10 +0100

    add: downloaded field on scrape summary

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,6 +1,6 @@
 export default {
   secret: 'lockless-broker',
-  jwtExpiration: 3600, // 1 hour
+  jwtExpiration: 3600 * 24 * 15, // 1 hour
   adminJwtExpiration: 3600 * 24 * 365 * 1000 , // 1 hour
   jwtRefreshExpiration: 86400 // 24 hours
 }
