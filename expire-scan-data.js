a31fb39baeed6ab03c8b04799cb16ba8e30af045
jgmts33 <jorgreemts@gmail.com>
2024-05-16 02:44:03 +0100

    add: expire feature of downloadibng

--- expire-scan-data.js
+++ expire-scan-data.js
@@ -1,5 +1,8 @@
 import { Sequelize } from "sequelize";
 import db from "./app/models/index.js";
+import fs from 'fs';
+import path from 'path';
+
 const { scrapeSummary: ScrapeSummary } = db;
 
 (async () => {
@@ -7,14 +10,35 @@ const { scrapeSummary: ScrapeSummary } = db;
   expirationDate.setDate(expirationDate.getDate() - 30); // Calculate 30 days ago
 
   try {
-    const expiredData = await ScrapeSummary.update(
-      { status: 'expired' },
-      { where: { createdAt: { [Sequelize.Op.lt]: expirationDate } } }
-    );
+    const expiredData = await ScrapeSummary.findAll({
+      where: {
+        createdAt: { [Sequelize.Op.lt]: expirationDate },
+        status: { [Sequelize.Op.ne]: 'expired' } // Exclude already expired data
+      },
+      attributes: ['id', 'scrape_date']
+    });
 
-    console.log("expiredData:", expiredData);
+    for (const data of expiredData) {
 
-    console.log(`Updated ${expiredData.length} records as expired.`);
+      try {
+
+        await ScrapeSummary.update(
+          { status: 'expired' },
+          { where: { id: data.id } }
+        );
+
+        await axios.post(`${process.env.BOT_API_ENDPOINT}/delete-file`, {
+          folder_name: data.scrape_date
+        });
+
+        console.log(`Removed ${data.scrape_date} record because that is expired.`);
+
+      } catch (err) {
+        console.log(err);
+      }
+
+    }
+
   } catch (error) {
     console.error('Error updating expired data:', error);
   }
