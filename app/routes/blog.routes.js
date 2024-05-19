4493064afdaa664cf92ee594da146435872701bb
jgmts33 <jorgreemts@gmail.com>
2024-05-19 01:03:02 +0100

    update: blog routes

--- app/routes/blog.routes.js
+++ app/routes/blog.routes.js
@@ -1,5 +1,6 @@
 import authJwt from "../middleware/authjwt.js";
 import { createBlog, deleteBlog, getBlog, getBlogs, updateBlog } from "../controllers/blog.controller.js";
+import upload from 'express-fileupload';
 
 export default function (app) {
 
@@ -10,6 +11,8 @@ export default function (app) {
     );
     next();
   });
+
+  app.use(upload());
 
   app.get(
     "/blogs",
