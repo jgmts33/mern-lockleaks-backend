b10ca3a68523697993caacdcd7a3d39f6d4cb6c3
jgmts33 <jorgreemts@gmail.com>
2024-05-24 02:09:54 +0100

    add: social-media-profiles api

--- app/controllers/social-media-profiles.controller.js
+++ app/controllers/social-media-profiles.controller.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 import archiver from 'archiver';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { Sequelize } from "sequelize";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
