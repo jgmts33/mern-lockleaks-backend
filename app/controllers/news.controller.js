bb1f8baf2c0ba2fbca2892c55b0a84277577b1bc
jgmts33 <jorgreemts@gmail.com>
2024-05-28 11:22:49 +0100

    add: newsletter feature

--- app/controllers/news.controller.js
+++ app/controllers/news.controller.js
@@ -2,7 +2,17 @@ import { Sequelize, Op } from "sequelize";
 import db from "../models/index.js";
 import path from 'path';
 
-const { news: News, subscribedUsers : SubscribedUsers } = db;
+const { news: News, subscribedUsers: SubscribedUsers } = db;
+
+let defaultClient = ElasticEmail.ApiClient.instance;
+
+import ElasticEmail from '@elasticemail/elasticemail-client';
+import elasticEmailConfig from '../config/elasticEmail.config..js';
+
+let apikey = defaultClient.authentications['apikey'];
+apikey.apiKey = "3D1F0FA1C5A6F371A302FE01088309D36EFFF8B9267447BEE543CEE904A4DD37ED7B42310822A28268C0278EF8D77F7C"
+
+let api = new ElasticEmail.EmailsApi()
 
 export const getNewsList = async (req, res) => {
 
@@ -111,12 +121,46 @@ export const sendNews = async (req, res) => {
 
   try {
 
-    const news = await News.findByPk(id); 
-    news.destroy();
+    const news = await News.findByPk(id);
 
-    res.status(200).send({
-      message: "News deleted Successfully!"
-    })
+    const emails = await SubscribedUsers.findAll({
+      attributes: ['email']
+    });
+
+    let Recipients = [];
+
+    for (const email of emails) {
+      Recipients.push(new ElasticEmail.EmailRecipient(email))
+    }
+
+    let emailContent = ElasticEmail.EmailMessageData.constructFromObject({
+      Recipients: Recipients,
+      Content: {
+        Body: [
+          ElasticEmail.BodyPart.constructFromObject({
+            ContentType: "HTML",
+            Content: news.content
+          })
+        ],
+        Subject: "Email Verification | LockLeaks",
+        From: elasticEmailConfig.auth.user,
+      }
+    });
+
+    var callback = function (error, data, response) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('API called successfully.');
+
+        res.status(200).send({
+          message: "News Sent Successfully!"
+        })
+
+      }
+    };
+
+    api.emailsPost(emailContent, callback);
 
   } catch (err) {
     res.status(500).send({
