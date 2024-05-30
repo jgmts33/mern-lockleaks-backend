c3f6bcba6da452d9518f915b00cd24bdcd1f2cc7
jgmts33 <jorgreemts@gmail.com>
2024-05-30 11:22:01 +0100

    add: ping models api

--- cron.js
+++ cron.js
@@ -7,105 +7,105 @@ const { scrapeSummary: ScrapeSummary, user: User, messages: Messages, tickets: T
 (async () => {
 
   // Setting the scraped data as expired after 30 days.
-  // try {
+  try {
 
-  //   const scrapedExpirationDate = new Date();
-  //   scrapedExpirationDate.setDate(scrapedExpirationDate.getDate() - 30); // Calculate 30 days ago
+    const scrapedExpirationDate = new Date();
+    scrapedExpirationDate.setDate(scrapedExpirationDate.getDate() - 30); // Calculate 30 days ago
 
-  //   const expiredData = await ScrapeSummary.findAll({
-  //     where: {
-  //       createdAt: { [Sequelize.Op.lt]: scrapedExpirationDate },
-  //       status: { [Sequelize.Op.ne]: 'expired' } // Exclude already expired data
-  //     },
-  //     attributes: ['id', 'scrape_date']
-  //   });
+    const expiredData = await ScrapeSummary.findAll({
+      where: {
+        createdAt: { [Sequelize.Op.lt]: scrapedExpirationDate },
+        status: { [Sequelize.Op.ne]: 'expired' } // Exclude already expired data
+      },
+      attributes: ['id', 'scrape_date']
+    });
 
-  //   for (const data of expiredData) {
+    for (const data of expiredData) {
 
-  //     try {
+      try {
 
-  //       await ScrapeSummary.update(
-  //         { status: 'expired' },
-  //         { where: { id: data.id } }
-  //       );
-  //       console.log("data.scrape_date:", data.scrape_date);
-  //       await axios.post(`https://api.lockleaks.com/delete-file`, {
-  //         folder_name: data.scrape_date
-  //       });
+        await ScrapeSummary.update(
+          { status: 'expired' },
+          { where: { id: data.id } }
+        );
+        console.log("data.scrape_date:", data.scrape_date);
+        await axios.post(`https://api.lockleaks.com/delete-file`, {
+          folder_name: data.scrape_date
+        });
 
-  //       console.log(`Removed ${data.scrape_date} record because that is expired.`);
+        console.log(`Removed ${data.scrape_date} record because that is expired.`);
 
-  //     } catch (err) {
-  //       console.log("err:", err);
-  //     }
+      } catch (err) {
+        console.log("err:", err);
+      }
 
-  //   }
+    }
 
-  // } catch (error) {
-  //   console.error('Error in setting the scraped data as expired after 30 days:', error);
-  // }
+  } catch (error) {
+    console.error('Error in setting the scraped data as expired after 30 days:', error);
+  }
 
-  // // Setting the Trial plan as expired after 3 days.
+  // Setting the Trial plan as expired after 3 days.
 
-  // try {
+  try {
 
-  //   const users = User.findAll({
-  //     where: {
-  //       subscription: {
-  //         [Sequelize.Op.and]: [
-  //           { status: { [Sequelize.Op.ne]: 'expired' } },
-  //           { expire_date: { [Sequelize.Op.lt]: new Date() } }
-  //         ]
-  //       }
-  //     }
-  //   });
+    const users = User.findAll({
+      where: {
+        subscription: {
+          [Sequelize.Op.and]: [
+            { status: { [Sequelize.Op.ne]: 'expired' } },
+            { expire_date: { [Sequelize.Op.lt]: new Date() } }
+          ]
+        }
+      }
+    });
 
-  //   for (const user of users) {
-  //     try {
-  //       await user.update({
-  //         subscription: {
-  //           ...user.subscription,
-  //           status: 'expired' // 'active'| 'expired'
-  //         }
-  //       });
+    for (const user of users) {
+      try {
+        await user.update({
+          subscription: {
+            ...user.subscription,
+            status: 'expired' // 'active'| 'expired'
+          }
+        });
 
-  //       console.log(`Trial Expired for the user:${user.id}`)
+        console.log(`Trial Expired for the user:${user.id}`)
 
-  //     } catch (err) {
-  //       console.log("err:", err);
-  //     }
-  //   }
+      } catch (err) {
+        console.log("err:", err);
+      }
+    }
 
-  // } catch (error) {
-  //   console.error('Error in setting the Trial plan as expired after 3 days.:', error);
-  // }
+  } catch (error) {
+    console.error('Error in setting the Trial plan as expired after 3 days.:', error);
+  }
 
-  // // Clear the Tickets and Messages after 30 days.
+  // Clear the Tickets and Messages after 30 days.
 
-  // try {
+  try {
 
-  //   const ticketExpirationDate = new Date();
-  //   ticketExpirationDate.setDate(ticketExpirationDate.getDate() - 30); // Calculate 30 days ago
+    const ticketExpirationDate = new Date();
+    ticketExpirationDate.setDate(ticketExpirationDate.getDate() - 30); // Calculate 30 days ago
 
-  //   const tickets = Tickets.findAll({
-  //     where: {
-  //       createdAt: { [Sequelize.Op.lt]: ticketExpirationDate },
-  //       status: { [Sequelize.Op.ne]: 'open' }
-  //     },
-  //   });
+    const tickets = Tickets.findAll({
+      where: {
+        createdAt: { [Sequelize.Op.lt]: ticketExpirationDate },
+        status: { [Sequelize.Op.ne]: 'open' }
+      },
+    });
 
-  //   for (const ticket of tickets) {
-  //     try {
-  //       await ticket.destroy();
-  //       console.log(`Tciket deleted after 30 days for the ticket:${ticket.id}`)
-  //     } catch (err) {
-  //       console.log("err:", err);
-  //     }
-  //   }
+    for (const ticket of tickets) {
+      try {
+        await ticket.destroy();
+        console.log(`Tciket deleted after 30 days for the ticket:${ticket.id}`)
+      } catch (err) {
+        console.log("err:", err);
+      }
+    }
 
-  // } catch (error) {
-  //   console.error('Error ing deleting tickets  after 30 days.:', error);
-  // }
+  } catch (error) {
+    console.error('Error ing deleting tickets  after 30 days.:', error);
+  }
 
   // Closing the Ticket after 6 days with no response.
 
